760b886f3c354dacd6a1febf199028db7ff3f54d
jianzaizai <1406088605@qq.com>
2014-11-16 11:53:32 +0000

    定义的函数不能再for循环里面

--- Test/C#入门经典/Chapter06/Ch06Ex08/Ch06Ex08/Program.cs
+++ Test/C#入门经典/Chapter06/Ch06Ex08/Ch06Ex08/Program.cs
@@ -7,26 +7,33 @@ namespace Ch06Ex08
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             int[] num = { -5, -11, 5, 0, 19, 30 };
+     
             int max;
             int min;
             Max(num, out max);
             Console.WriteLine(max);
             Min(num, out min);
-            Console.WriteLine(max);
+            Console.WriteLine(min);
             Console.ReadKey();
-            static void  Max(int[] num,out int max)
+        }
+        static void Max(int[] num, out int max)
+        {
+            int m = 0;
+            for (int i = 0; i < num.Length; i++)
             {
-                int m = 0;
-                for(int i = 0;i < num.Length;i++)
-                if(m<num[i])
+                if (m < num[i])
                 {
                     m = num[i];
                 }
-                max = m;
             }
+
+            max = m;
+        }
+
         public static void Min(int[] num, out int min)
         {
             int m = 0;
@@ -36,9 +43,10 @@ namespace Ch06Ex08
                 {
                     m = num[i];
                 }
-                min = m;
+             
             }
+               min = m;
          }
-        }
-    }
+      }
 }
+
