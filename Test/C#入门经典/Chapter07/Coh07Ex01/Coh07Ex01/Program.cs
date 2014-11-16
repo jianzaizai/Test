8953a5a6da8b4e350116f0d3f65a91b5de15715b
jianzaizai <1406088605@qq.com>
2014-11-16 20:48:23 +0800

    /// <summary>         /// 计数         /// </summary>         static int count;
            /// <summary>
            /// 加法
            /// </summary>

--- Test/C#入门经典/Chapter07/Coh07Ex01/Coh07Ex01/Program.cs
+++ Test/C#入门经典/Chapter07/Coh07Ex01/Coh07Ex01/Program.cs
@@ -5,47 +5,68 @@ using System.Text;
 
 namespace Coh07Ex01
 {
-   /* class Program
-    {
-        //通过字段来描述类别信息的变量
-        private string name;
-        public string Name
-        {
-            get {return name;}
-            set {name = value;}
-        }
-        public string gender;
-        public int age;
-        public string address;
-        public string phone;
-        //通过方法来描述类的动作
-        public void Eat()
-        {
-            Console.WriteLine("我在吃东西");
-        }
-        public void Sport()
-        {
-            Console.WriteLine("吃完一起健身");
-            Console.ReadKey();
+    /* class Program
+     {
+         //通过字段来描述类别信息的变量
+         private string name;
+         public string Name
+         {
+             get {return name;}
+             set {name = value;}
+         }
+         public string gender;
+         public int age;
+         public string address;
+         public string phone;
+         //通过方法来描述类的动作
+         public void Eat()
+         {
+             Console.WriteLine("我在吃东西");
+         }
+         public void Sport()
+         {
+             Console.WriteLine("吃完一起健身");
+             Console.ReadKey();
 
-        }
-    }
-}*/
-/*namespace_001
-{*/
+         }
+     }
+ }*/
+    /*namespace_001
+    {*/
     class Program
     {
+        /// <summary>
+        /// 计数
+        /// </summary>
+        static int count;
+
+        /// <summary>
+        /// 加法
+        /// </summary>
+        static int Add(int x,int y)
+        {
+            return x + y;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            count = 0;
+            Console.WriteLine(Add(1,2));
             //类的实例化
-            Person w = new Person() {Name="Join",gender="女",address="郑州",age=13,phone="321321312"};
-            w.Eat();
-            w.Sport();
-            Console.WriteLine("我叫{0},是个{1}生,我住在{2},今年{3}岁,我的联系方式是{4}",w.Name,w.gender,w.address,w.age,w.phone);
-            Console.ReadKey();
-        /*static void Main(string[] args)
-        {
+            //Person w = new Person() { Name = "Join", gender = "女", address = "郑州", age = 13, phone = "321321312" };
+            //w.Eat();
+            //w.Sport();
+            //Console.WriteLine("我叫{0},是个{1}生,我住在{2},今年{3}岁,我的联系方式是{4}", w.Name, w.gender, w.address, w.age, w.phone);
+            //Console.ReadKey();
+            /*static void Main(string[] args)
+            {
 
-        }*/
+            }*/
+            Console.ReadLine();
+        }
     }
-   }
+}
--- Test/C#入门经典/Chapter07/Coh07Ex02/Program.cs
+++ Test/C#入门经典/Chapter07/Coh07Ex02/Program.cs
@@ -7,55 +7,55 @@ namespace Coh07Ex02
 {
     class Program
     {
-        string Name;
-        public string Name
-        {
-            get { return name;}
-            set {name = value;}
-        }
-        string gender;
-        public string gender
-        {
-            get {return gender;}
-            set {gender = value;}
-        } 
-        int age;
-        public int Age
-        {
-            get {return age;}
-            set {age = 20;}   
-        }
-        int chinese;
-        public int chinese
-        {
-            get {return chinese;}
-            set {chinese = value;}
-        }
-        int math;
-        public int math
-        {
-            get {return math;}
-            set {math = value;}
-        }
-        int english;
-        public interface english
-        {
-            get {return english;}
-            set {english = value;}
-        }
-        public void SayHi()
-        {
-            console.writeLine("我叫{0},今年{1}岁,是{2}同学",name,age,gender);
-        }
-        public void Score()
-        {
-            int erface sum = 0;
-            int avg = 0;
-            sum = chinese + Math + english;
-            avg = sum / 3;
-            console.WriteLine("我是{0},考试总成绩{1},平均成绩{2}",name,sum,avg)
+    //    string Name;
+    //    public string Name
+    //    {
+    //        get { return name;}
+    //        set {name = value;}
+    //    }
+    //    string gender;
+    //    public string gender
+    //    {
+    //        get {return gender;}
+    //        set {gender = value;}
+    //    } 
+    //    int age;
+    //    public int Age
+    //    {
+    //        get {return age;}
+    //        set {age = 20;}   
+    //    }
+    //    int chinese;
+    //    public int chinese
+    //    {
+    //        get {return chinese;}
+    //        set {chinese = value;}
+    //    }
+    //    int math;
+    //    public int math
+    //    {
+    //        get {return math;}
+    //        set {math = value;}
+    //    }
+    //    int english;
+    //    public interface english
+    //    {
+    //        get {return english;}
+    //        set {english = value;}
+    //    }
+    //    public void SayHi()
+    //    {
+    //        console.writeLine("我叫{0},今年{1}岁,是{2}同学",name,age,gender);
+    //    }
+    //    public void Score()
+    //    {
+    //        int erface sum = 0;
+    //        int avg = 0;
+    //        sum = chinese + Math + english;
+    //        avg = sum / 3;
+    //        console.WriteLine("我是{0},考试总成绩{1},平均成绩{2}",name,sum,avg)
 
-        }
+    //    }
         static void Main(string[] args)
         {
 
