1cef0426bb3523b2594b4017e811ec340077cf05
jianzaizai <1406088605@qq.com>
2014-11-16 09:36:54 +0000

    c#入门经典

---
+++ Test/C#入门经典/Chapter05/Ch05Ex06/Ch05Ex06/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch05Ex06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一连串字母：")；
+                string
+        }
+    }
+}
