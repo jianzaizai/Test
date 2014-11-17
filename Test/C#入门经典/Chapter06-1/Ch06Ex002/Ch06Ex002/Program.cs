7bdbb8c8c9098d81b90ff460bbf7fbd6da5cecf6
jianzaizai <1406088605@qq.com>
2014-11-17 10:12:34 +0800

    通过函数交换数据

---
+++ Test/C#入门经典/Chapter06-1/Ch06Ex002/Ch06Ex002/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch06Ex002
+{
+    class Program
+    {
+        static int MaxValue(int[] intArray)
+        {
+            //数组
+            int maxVal = intArray[0];
+            
+            for (int i = 1; i < intArray.Length; i++)
+            {
+                //判断值的大小
+                if (intArray[i] > maxVal)
+                    maxVal = intArray[i];
+            }
+            return maxVal;
+        }
+        static void Main(string[] args)
+        { 
+            //数组赋初值
+            int[] myArray = { 1, 8, 3, 6, 2, 5, 9, 3, 0, 2 };
+            int maxVal = MaxValue(myArray);
+            Console.WriteLine("The maxium value in myArray is {0}", maxVal);
+            Console.ReadKey();
+        }
+    }
+}
