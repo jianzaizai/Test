2b894b21b500dfa817bbcc48758b01a854f5e0dc
jianzaizai <1406088605@qq.com>
2014-11-17 12:50:03 +0800

    定义和使用基本函数

---
+++ Test/C#入门经典/Chapter06-1/Ch06Ex004/Ch06Ex004/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch06Ex004
+{
+    class Program
+    {
+        static void Write()   //Write()定义初始化了mystring
+                             //与Main()定义的mystring不同
+        {
+            string myString = "String defined in Write()";
+            Console.WriteLine("Now in Write()");  
+            Console.WriteLine("myString = {0}", myString);
+        }
+
+        static void Main(string[] args)
+        { 
+            //Main()定义初始化变量string,Main()把控制权给了Write()
+            string myString = "String defined in Main()";
+            Write();
+            Console.WriteLine("\nNow in Main()");
+            Console.WriteLine("mystring = {0}", myString);
+        }
+    }
+}
