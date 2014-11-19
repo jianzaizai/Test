1a1ca7eaf9f9202b0191d9717d20c244d3fbb284
jianzaizai <1406088605@qq.com>
2014-11-19 10:27:38 +0800

    类和结构

---
+++ Test/C#入门经典/Chapter09/Ch09Ex02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch09Ex02
+{
+    //类MyClass
+    class MyClass
+    {
+        public int val;  //公共的int字段val
+    }
+
+    //结构myStruct
+    struct myStruct
+    {
+        public int val;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass objectA = new MyClass();
+            //把第一个对象引用赋给类型为myclass的第二个变量
+            MyClass objectB = objectA;
+            objectA.val = 10;
+            objectB.val = 20;
+            myStruct structA = new myStruct();
+            //把第一个结构赋类型为mystruct第二个变量
+            myStruct structB = structA;
+            structA.val = 30;
+            structB.val = 40;
+            Console.WriteLine("objectA.val = {0}", objectA.val);
+            Console.WriteLine("objectB.val = {0}", objectB.val);
+            Console.WriteLine("structA.val = {0}", structA.val);
+            Console.WriteLine("structB.val = {0}", structB.val);
+            Console.ReadKey();
+        }
+    }
+}
