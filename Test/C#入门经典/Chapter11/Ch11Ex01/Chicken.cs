37a8cf153e5472ae8a573f9a0e1ea41fe227f52c
jianzaizai <1406088605@qq.com>
2014-11-22 17:58:14 +0800

    数组和高级集合

---
+++ Test/C#入门经典/Chapter11/Ch11Ex01/Chicken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ch11Ex01
+{
+    public class Chicken : Animal 
+    {
+        public void LayEgg()
+        {
+            Console.Write("{0} has laid an egg.", name);
+        }
+        public Chicken(string newName)  : base(newName)
+        { 
+        }
+    }
+}
